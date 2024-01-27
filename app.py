680a34c9076a8710374b0718701f87da0680c4a3
Faer-faer <ruslan020507@gmail.com>
2024-01-27 03:49:27 +0300

    commit
    add folders and app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+from flask import Flask, render_template, redirect, url_for, request
 
+import mysql.connector
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+sql = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="121377Rr.",
+    database="HACKOUT"
+)
+
+cursor = sql.cursor()
+
+
+@app.route('/')
+@app.route('/hackout', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+if '__main__' == __name__:
+    app.run(debug=True)
